17c49477872bcee665c3c499080b6d3cb52c1ed3
mayurawari <117013117+mayurawari@users.noreply.github.com>
2024-08-23 21:07:43 +0530

    updated-responsivenes

--- index.js
+++ index.js
@@ -7,62 +7,20 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-let hambtn = document.getElementById("menu-toggle");
-let menuBox = document.getElementById("nav-list-items");
-let hamele = document.getElementById("ham-list-items");
+let ele1 = document.getElementById("about-png");
+let ele2 = document.getElementById("about-text");
 
-window.addEventListener("resize", () => {
-  let width = window.outerWidth;
-  if (width < 700) {
-    hambtn.style.display = "flex";
-    document.getElementById("hire-btn").style.display = "none";
-    menuBox.style.display = "none";
-}
- else {
-    hambtn.style.display = "none";
-    document.getElementById("ham-box").style.display = "none"
-    document.getElementById("hire-btn").style.display = "flex";
-    menuBox.style.display = "flex";
+window.addEventListener("scroll", () => {
+  let height = document.documentElement.scrollTop;
+  if (height > 400) {
+    ele1.style.animation = "slideleft 1s ease-in-out forwards";
+    ele2.style.animation = "slideright 1s ease-in-out forwards";
   }
 });
 
-hambtn.addEventListener("click", () => {
-if(document.getElementById("ham-box").style.display == "none" && window.outerWidth <700){
-
-    document.getElementById("ham-box").style.display = "block";
-    document.getElementById("ham-box").style.width = "180px";
-    document.getElementById("ham-box").style.height = "300px";
-    document.getElementById("ham-box").style.backgroundColor = "black";
-    document.getElementById("ham-box").style.position = "relative";
-    document.getElementById("ham-box").style.top = "200px";
-
-    hamele.style.display = "flex";
-    hamele.style.gap = "30px";
-    hamele.style.flexDirection = "column";
-    hamele.style.justifyContent = "center";
-    hamele.style.alignItems = "center";
-    hamele.style.display = "none";
-    // let items=document.getElementById("ham-list-items");
-    // items.style.color='white';
-}
-else{
-    document.getElementById("ham-box").style.display = "none"
-}
+document.getElementById("resume-link").addEventListener("click", function () {
+  setTimeout(function () {
+    window.location.href =
+      "https://drive.google.com/uc?export=download&id=1as2SCjjSh8lZDckK2o8MWuJX2N3UBVqc";
+  }, 2000);
 });
-
-
-
-let ele1=document.getElementById("about-png");
-let ele2=document.getElementById("about-text");
-
-
-window.addEventListener("scroll",()=>{
-  let height=document.documentElement.scrollTop;
-  if(height>400){
-    ele1.style.animation="slideleft 1s ease-in-out forwards";
-    ele2.style.animation="slideright 1s ease-in-out forwards";
-  }
-})
-
-
-
